211120451faa3584204cbed89d591852445ddb1d
alper yavuz ün <yavuzun50@hotmail.com>
2024-04-12 22:38:16 +0300

    functions

---
+++ PythonPractices/functions.py
@@ -0,0 +1,27 @@
+def hello():
+    print("Hello world!")
+
+hello()
+
+def sum(num1=0, num2=0):
+    if(type(num1) is not int or type(num2) is not int):
+        return # or return 0
+    return num1 + num2
+
+total = sum(34,67)
+print(total)
+
+total = sum()
+print(total)
+
+def multiple_items(*args):
+    print(args)
+    print(type(args))
+
+multiple_items("Dave", "John","Sara")
+
+def mult_named_items(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+mult_named_items(first="Dave", last="Gray")
